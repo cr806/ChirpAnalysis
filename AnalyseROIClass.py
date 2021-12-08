7bd9f7310a3f9b564cef1e92d89bb9e2e5bab4df
Christopher <christopher.p.reardon@gmail.com>
2021-12-08 10:38:25 +0000

    Altered ROI class, to allow passing in of angle, ROI and resonance information.

--- AnalyseROIClass.py
+++ AnalyseROIClass.py
@@ -10,25 +10,43 @@ from Functions import ROI, convert_CSV_to_Image
 
 config = configparser.ConfigParser()
 config.read('./CK_configuration.ini')
-print(config)
 
-save_path = config['Config']['save_path']
-save_name = config['Config']['save_name']
+save_path = config['Data']['save_path']
+save_name = config['Data']['save_name']
 
-image_folder = config['Config']['image_folder']
-image_type = config['Config']['image_type']
-image_align = config['Config']['image_align']
-save_figure_filename = config['Config']['save_figure_filename']
+image_folder = config['Data']['image_folder']
+image_type = config['Data']['image_type']
+image_align = config['Data']['image_align']
+save_figure_filename = config['Data']['save_figure_filename']
 
-sleep_time = config['Config'].getfloat('sleep_time')
+try:
+    if config['Data'].getboolean('live_watch'):
+        sleep_time = config['Loop'].getfloat('sleep_time')
+except KeyError:
+    print("You have not provided a sleep time, it is now set to 10 minutes")
+    sleep_time = 10
 
-num_of_ROIs = config['Config'].getint('num_of_ROIs')
+try:
+    num_of_ROIs = config['Image'].getint('num_of_ROIs')
+    image_interval = config['Image'].getfloat('image_interval')
+    if config['Data'].getboolean('initialise_image'):
+        angle = config['Image'].getfloat('image_angle')
+except KeyError:
+    num_of_ROIs = 1
+    image_interval = 1
+    angle = None
 
+roi_ranges = [None, None]
+res_gamma = [None, None]
 roi_array = []
-first_image_processed = []
 for roi in range(num_of_ROIs):
-    roi_array.append(ROI())
-    first_image_processed.append(False)
+    try:
+        roi_range = roi_ranges[roi]
+        r_g = res_gamma[roi]
+    except IndexError:
+        roi_range = None
+        r_g = None
+    roi_array.append(ROI(angle=angle, roi=roi_range, res=r_g))
 
 # filenames_old = listdir(image_folder)
 # print(f'Original filenames -> \n {sorted(filenames_old)}')
@@ -40,11 +58,11 @@ for i in range(1):
 
     image_files = [join(image_folder, f) for f in sorted(to_be_processed)
                    if f[-3:] == image_type]
-
+    print(i)
     # print(to_be_processed)
     # print(image_files)
+    # image_files = image_files[:3]
     print(f'{len(image_files)} files will now be processed')
-    image_files = image_files[:1]
 
     for idx, im_path in enumerate(image_files):
         print(f'Processing image {idx}')
@@ -64,10 +82,7 @@ for i in range(1):
         print(f'Loading data took: {(end - start):.4} s')
 
         for idx, roi in enumerate(roi_array):
-            if not first_image_processed[idx]:
-                roi.set_initial_ROI(im)
-                roi.create_ROI_data(im)
-                first_image_processed[idx] = True
+            roi.set_initial_ROI(im)
             roi.create_ROI_data(im)
             roi.process_ROI()
             # roi.get_resonance_data(disp=True)
@@ -76,19 +91,17 @@ for i in range(1):
         roi.save_data(save_path, f'{save_name}_ROI_{idx}')
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 6))
+    time_axis = [(y * image_interval) for y in range(len(filenames_old)-1)]
     for roi in roi_array:
         res_data = [d['res'] for d in roi.get_resonance_data()]
         FWHM_data = [d['FWHM'] for d in roi.get_resonance_data()]
-        r2_data = [round(d['r2'], 2) for d in roi.get_resonance_data()]
-        ax1.plot(res_data)
-        ax2.plot(FWHM_data)
-        ax3.plot(r2_data)
-        # ax1.set_xlim(0, 50)
-        # ax1.set_ylim(0, 300)
-        # ax2.set_xlim(0, 50)
-        # ax2.set_ylim(60, 150)
-        # ax3.set_xlim(0, 50)
-        # ax3.set_ylim(0.98, 1.01)
+        r2_data = [round(d['r2'], 4) for d in roi.get_resonance_data()]
+        ax1.plot(time_axis, res_data)
+        ax2.plot(time_axis, FWHM_data)
+        ax3.plot(time_axis, r2_data)
+        ax1.set_xlabel('Time (minutes)')
+        ax2.set_xlabel('Time (minutes)')
+        ax3.set_xlabel('Time (minutes)')
         plt.savefig(save_figure_filename)
 
     print('Waiting ...')
