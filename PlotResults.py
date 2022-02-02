d276652b317c34a9b2af245427a822eef4dea1af
Christopher <christopher.p.reardon@gmail.com>
2022-02-02 16:54:55 +0000

    Clean up repository, add time_interval into conf file for reference

--- PlotResults.py
+++ PlotResults.py
@@ -8,7 +8,7 @@ image_align = 'vertical'
 data, num_subROIs = fnc.get_data(data_filename)
 num_of_results = len(data['ROI_x1'])
 
-fig, ax = plt.subplots(1, 5, figsize=(12, 6))
+fig, ax = plt.subplots(1, 5, figsize=(12, 6), constrained_layout=True)
 colours = ('b', 'g', 'r', 'c', 'm', 'k')
 
 for idx, subROI in enumerate(subROIs_to_plot):
@@ -28,30 +28,29 @@ for idx, subROI in enumerate(subROIs_to_plot):
                color=colours[idx % 6],
                label=f'subROI-{subROI}')
     ax[0].set_title('Resonance location')
-    ax[0].legend()
 
     ax[1].plot(gamma,
                color=colours[idx % 6],
                label=f'subROI-{subROI}')
     ax[1].set_title('Gamma')
-    ax[1].legend()
 
     ax[2].plot(off,
                color=colours[idx % 6],
                label=f'subROI-{subROI}')
     ax[2].set_title('Offset')
-    ax[2].legend()
 
     ax[3].plot(fwhm,
                color=colours[idx % 6],
                label=f'subROI-{subROI}')
     ax[3].set_title('FWHM')
-    ax[3].legend()
 
     ax[4].plot(r2,
                color=colours[idx % 6],
                label=f'subROI-{subROI}')
     ax[4].set_title('R2')
-    ax[4].legend()
+    ax[4].legend(loc='center right')
+
+    plt.subplots_adjust(bottom=0.1, left=0.1)
+    plt.pause(0.5)
 
 plt.show()
