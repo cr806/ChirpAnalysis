3b52cd7d1a85b05678d8fdb6e91e45b01256d2c3
cr806 <christopher.reardon@york.ac.uk>
2023-12-15 10:35:45 +0000

    Update plotting script

--- PlotResults.py
+++ PlotResults.py
@@ -1,25 +1,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data_filename1 = 'Libra_Data_20230810-000004_ROI1_Fano.csv'
-data_filename2 = 'Libra_Data_20230810-000004_ROI1_MedianOfFano.csv'
+file_details = [['CentreOfPeak', 'MedianOfCentres', 'Centre'],
+               ['Gaussian', 'MedianOfGaussian', 'Mu'],
+               ['Fano', 'MedianOfFano', 'Resonance']
+               ]
 
-data1 = pd.read_csv(data_filename1)
-data2 = pd.read_csv(data_filename2)
+for file1, file2, column in file_details:
+    save_name = f'Libra_20230810-000004-{file1}.png'
 
-subROIs = data1['subROI'].unique()
-print(subROIs[0])
+    data_filename1 = f'Libra_Data_20230810-000004_ROI1_{file1}.csv'
+    data_filename2 = f'Libra_Data_20230810-000004_ROI1_{file2}.csv'
 
-x_data = range(0, len(data2['Median']))
+    data1 = pd.read_csv(data_filename1)
+    data2 = pd.read_csv(data_filename2)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-# colours = ('b', 'g', 'r', 'c', 'm', 'k')
-for idx in subROIs:
-    y_data = data1[data1['subROI'] == subROIs[idx]]['Resonance']
-    ax1.plot(y_data, label=f'SubROI: {idx}')
-ax1.legend()
-ax1.set_title('SubROI analysis')
-ax2.plot(x_data,data2['Median'])
-ax2.fill_between(x_data, data2['First Quartile'], data2['Third Quartile'], alpha=0.2)
-ax2.set_title('Median analysis')
-plt.show()
+    subROIs = data1['subROI'].unique()
+
+    x_data = range(0, len(data2['Median']))
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    for idx in subROIs:
+        y_data = data1[data1['subROI'] == subROIs[idx]][column]
+        ax1.plot(x_data, y_data, label=f'SubROI: {idx}')
+    ax1.legend()
+    ax1.set_ylim([170, 240])
+    ax1.set_title('SubROI analysis')
+    ax2.plot(x_data,data2['Median'])
+    ax2.fill_between(x_data, data2['First Quartile'], data2['Third Quartile'], alpha=0.2)
+    ax2.set_ylim([170, 240])
+    ax2.set_title('Median analysis')
+    fig.suptitle(f'Analysis method: {file1}')
+    fig.savefig(save_name)
+    # plt.pause(2)
