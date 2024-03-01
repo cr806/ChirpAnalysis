eee68e3989f56920b78b90fa43cc4f82ec96b1be
cr806 <christopher.reardon@york.ac.uk>
2024-03-01 14:31:13 +0000

    Check/create save directory

--- AnalyseChirpGMRS.py
+++ AnalyseChirpGMRS.py
@@ -1,5 +1,6 @@
 import time
 import logging
+import os
 import Functions as func
 from datetime import datetime
 
@@ -12,6 +13,18 @@ logger = func.setup_logger(log_filepath, log_level)
 params = func.get_simulation_params(logger, config_filepath)
 params['log_filepath'] = log_filepath
 params['log_level'] = log_level
+
+params['save_path']
+
+# Creates the save directory if it doesn't already exist.
+try:
+    os.makedirs(params['save_path'], exist_ok=True)
+    print(f'Directory {params['save_path']} available.')
+    logger.info(f'Directory {params['save_path']} available.')
+except OSError as e:
+    print(f'Error with save directory {params['save_path']}: {e}')
+    logger.error(f'Error with save directory {params['save_path']}: {e}')
+    exit(1)
 
 roi_array = None
 
